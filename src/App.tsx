6de08df9fe7f69af95b3d51a133d3cc5b8b701ac
benoit gosset <benoit.gosset@relyfe.com>
2022-03-16 16:05:56 +0100

    feat(App): display video

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import porcheVideo from './assets/porshe.mp4';
+
 const App: React.FC = () => {
   return (
-    <></>
+    <video controls id="video">
+      <source src={porcheVideo} type="video/mp4"/>
+    </video>
   );
 }
 
